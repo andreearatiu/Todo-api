d070175c9fd67a8b32fd1e070de372fabb250e3f
Andreea Ratiu <ratiu.andreea.ioana@gmail.com>
2016-03-17 16:54:24 +0200

    Replaced description

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [ {
 	id: 1,
-	description: 'Meet tud for dinner',
+	description: 'Pick up dad',
 	completed: false
 }, {
 	id: 2, 
