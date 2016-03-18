427133e4dba66999d19e621e13f76a084307ac5a
Andreea Ratiu <ratiu.andreea.ioana@gmail.com>
2016-03-18 13:13:34 +0200

    Changed 404 message

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/todos/:id', function (req, res) {
 	if(matchedToDo) {
 		res.json(matchedToDo);
 	} else {
-		res.status(404).send('This id does not exist !');
+		res.status(404).send();
 	}
 	
 });
