6329070bb56e58e6f6ae34f30ae7fc579faac300
Andreea Ratiu <ratiu.andreea.ioana@gmail.com>
2016-03-22 15:31:32 +0200

    Final static todos

---
+++ variable-playground.js
@@ -0,0 +1,27 @@
+// var person = {
+// 	name: 'Andrew',
+// 	age: 21
+// }
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Andrew',
+// 	// 	age: 24
+// 	// }
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+//Array example
+var grades = [15, 27];
+
+function addGrade (obj) {
+	//obj = [15, 27, 4];
+	obj.push(2);
+	debugger;
+}
+
+addGrade(grades);
+console.log(grades);
